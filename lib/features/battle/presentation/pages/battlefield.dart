1121907054fbd1f4fa9fc39c8f6439fe79502179
viniciusdomg <viniciusmgomes2005@hotmail.com>
2025-10-21 09:31:23 -0300

    feat: criando as telas principais

---
+++ lib/features/battle/presentation/pages/battlefield.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Battlefield extends StatelessWidget {
+  const Battlefield({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/heroes/presentation/pages/heroes_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HeroesList extends StatelessWidget {
+  const HeroesList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
